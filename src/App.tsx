917fe80e953528c8327ba8886af7b300b5201bd2
Edgardo <edgardomay@outlook.com>
2026-05-30 01:25:08 -0500

    feat: implement App routing and initialize design skill configurations and documentation

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,9 @@ import { Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import ToursCatalog from './components/Tours/ToursCatalog';
 import TourDetailPagePro from './components/Tours/TourDetailPagePro';
+import PoliticasCancelacion from './pages/PoliticasCancelacion';
+import TerminosCondiciones from './pages/TerminosCondiciones';
+import ExencionResponsabilidad from './pages/ExencionResponsabilidad';
 
 const App: FC = () => {
   return (
@@ -10,6 +13,9 @@ const App: FC = () => {
       <Route path="/" element={<Home />} />
       <Route path="/tours" element={<ToursCatalog />} />
       <Route path="/tours/:id" element={<TourDetailPagePro />} />
+      <Route path="/politicas-de-cancelacion" element={<PoliticasCancelacion />} />
+      <Route path="/terminos-y-condiciones" element={<TerminosCondiciones />} />
+      <Route path="/exencion-de-responsabilidad" element={<ExencionResponsabilidad />} />
     </Routes>
   );
 };
