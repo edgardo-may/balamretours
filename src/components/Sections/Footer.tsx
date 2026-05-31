138dbd79cf3f78a61e51d947fe693313e8bcaba2
Edgardo <edgardomay@outlook.com>
2026-05-31 03:42:50 -0500

    feat: add design Footer component

--- src/components/Sections/Footer.tsx
+++ src/components/Sections/Footer.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { MapPin, Mail, Phone } from "lucide-react";
-import { FaFacebook, FaInstagram, FaXTwitter } from "react-icons/fa6";
+import { FaFacebook, FaInstagram } from "react-icons/fa6";
 import { Link } from "react-router-dom";
 
 const Footer: React.FC = () => {
